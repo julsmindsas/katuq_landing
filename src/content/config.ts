3f5fc6a6c482320061565c4846a7c422cbf5e120
julsmindsas <dgarciah@julsmind.com>
2024-12-04 19:02:00 -0500

    update

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,35 @@
-import { z, defineCollection } from 'astro:content'
+// import { z, defineCollection } from 'astro:content'
+
+// const blogCollection = defineCollection({
+// 	schema: () =>
+// 		z.object({
+// 			title: z.string(),
+// 			description: z.string(),
+// 			pubDate: z.date(),
+// 			image: z.string(),
+// 			author: z.string(),
+// 			tags: z.array(z.string())
+// 		})
+// })
+
+// export const collections = {
+// 	posts: blogCollection
+// }
+
+import { z, defineCollection } from 'astro:content';
 
 const blogCollection = defineCollection({
-	schema: () =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			pubDate: z.date(),
-			image: z.string(),
-			author: z.string(),
-			tags: z.array(z.string())
-		})
-})
+  schema: () =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.date(),
+      image: z.string(),
+      author: z.string(),
+      tags: z.array(z.string()),
+    }),
+});
 
 export const collections = {
-	posts: blogCollection
-}
+  blog: blogCollection,
+};
